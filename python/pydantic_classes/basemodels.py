d3904d1ce8426e0ffe411b6ee0aabb0e8ad8cc6c
Shresth-Agarwal <7shresthagarwal@gmail.com>
2026-01-10 01:14:07 +0530

    added basemodels

---
+++ python/pydantic_classes/basemodels.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+from typing import List
+
+class SalesHistory(BaseModel):
+    sales_history: List[float]
+
+class ForecastResponse(BaseModel):
+    forecast: int
+    confidence: float
+
+class DecisionPayload(BaseModel):
+    forecast: int
+    confidence: float
+    current_stock: int
+    unit_cost: float
+
+class ForecastAndDecideRequest(SalesHistory):
+    current_stock: int
+    unit_cost: float
+    
+class ChatRequest(BaseModel):
+    user_input: str
