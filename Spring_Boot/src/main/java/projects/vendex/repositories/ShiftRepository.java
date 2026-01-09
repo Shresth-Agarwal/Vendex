dd5f93e030ff9977f1652359a48c4961ae79c48d
Risspecct <rishikgarg.dev@gmail.com>
2026-01-10 01:53:30 +0530

    updated shot repository

--- Spring_Boot/src/main/java/projects/vendex/repositories/ShiftRepository.java
+++ Spring_Boot/src/main/java/projects/vendex/repositories/ShiftRepository.java
@@ -3,8 +3,10 @@ package projects.vendex.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import projects.vendex.entities.Shift;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface ShiftRepository extends JpaRepository<Shift, Long> {
     List<Shift> findByStatus(String status);
+    List<Shift> findByStatusAndShiftDate(String status, LocalDate shiftDate);
 }
